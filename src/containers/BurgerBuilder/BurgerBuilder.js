721339c956c54bbef8d891439dc8459014c0db5d
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-05-10 23:37:54 -0400

    add data to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-order";
 
 const INGREDIENT_PRICE = {
   salad: 0.5,
@@ -68,7 +69,24 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You can continue !");
+    //alert("You can continue !");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Larry",
+        address: {
+          street: "test1 St",
+          zipCode: 11101,
+        },
+        email: "test1@test.com",
+        deliveryMethod: "fastest",
+      },
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
   };
   // add methods;
   render() {
